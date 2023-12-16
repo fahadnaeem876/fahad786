74d15a2e9a001f5b1a333fca45567e2311c03fe3
muhammad fahad <muhammadfahadnaeem4@gmail.com>
2023-12-16 14:30:21 +0500

    initial commit

--- languagetranslator.py
+++ languagetranslator.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request ,Blueprint
+from flask import Flask, render_template, request, Blueprint
 from googletrans import Translator
 
-languagetranslator_app = Blueprint("languagetranslator",__name__)
+languagetranslator_app = Blueprint("languagetranslator", __name__)
 
 # Language names and codes
 language_names = {
-    "English": "English",
+    "English": "english",
     "Spanish": "Spanish",
     "French": "French",
     "Hindi": "Hindi",
     "German": "German",
     "Japanese": "Japanese",
-    "Urdu": "urdu",
+    "Urdu": "Urdu",
     "Arabic": "Arabic"
 }
 
@@ -23,10 +23,31 @@ def index():
         to_lang = request.form.get("to_lang")
 
         translator = Translator()
-        translated_text = translator.translate(user_input, src=from_lang, dest=to_lang).text
+        try:
+            translation = translator.translate(user_input, src=from_lang, dest=to_lang)
+            translated_text = translation.text if translation else "Translation failed. Please try again."
+        except Exception as e:
+            print(f"Translation error: {e}")
+            translated_text = "An error occurred during translation."
 
-        return render_template("languagetranslator.html", translated_text=translated_text, user_input=user_input, from_lang=from_lang, to_lang=to_lang, language_names=language_names)
+        return render_template(
+            "languagetranslator.html",
+            translated_text=translated_text,
+            user_input=user_input,
+            from_lang=from_lang,
+            to_lang=to_lang,
+            language_names=language_names
+        )
 
-    return render_template("languagetranslator.html", translated_text=None, user_input=None, from_lang=None, to_lang=None, language_names=language_names)
+    return render_template(
+        "languagetranslator.html",
+        translated_text=None,
+        user_input=None,
+        from_lang=None,
+        to_lang=None,
+        language_names=language_names
+    )
 
-
+    app = Flask(__name__)
+    app.register_blueprint(languagetranslator_app, url_prefix="/languagetranslator")
+    app.run(debug=True)
