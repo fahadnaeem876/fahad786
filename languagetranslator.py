2ceb1b8202c1b03b61821e8e227528b060375fa2
muhammad fahad <muhammadfahadnaeem4@gmail.com>
2023-12-18 17:08:39 +0500

    initial commit

--- languagetranslator.py
+++ languagetranslator.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, Blueprint
-from googletrans import Translator
+from googletrans import Translator, LANGUAGES
 
-languagetranslator_app = Blueprint("languagetranslator",__name__)
+languagetranslator_app = Blueprint("languagetranslator", __name__)
 
 @languagetranslator_app.route('/', methods=['GET', 'POST'])
 def translate_text():
@@ -12,13 +12,17 @@ def translate_text():
         translator = Translator()
 
         try:
-            detected_language = translator.detect(text_to_translate).lang
+            # Detect the language
+            detected_language = translator.detect(text_to_translate)
+            detected_language_code = detected_language.lang if detected_language else None
+
+            # Translate the text
             translated_text = translator.translate(text_to_translate, dest=target_language).text
 
-            return render_template('languagetranslator.html', 
-                                   original_text=text_to_translate, 
-                                   detected_language=detected_language,
-                                   target_language=target_language,
+            return render_template('languagetranslator.html',
+                                   original_text=text_to_translate,
+                                   detected_language=LANGUAGES.get(detected_language_code, "Unknown"),
+                                   target_language=LANGUAGES.get(target_language, "Unknown"),
                                    translated_text=translated_text)
         except Exception as e:
             error_message = f"Error: {e}"
