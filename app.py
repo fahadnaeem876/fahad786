733cbbb98927891c9d18075ca02cfd7b3c2f17c5
muhammad fahad <muhammadfahadnaeem4@gmail.com>
2023-12-18 14:19:33 +0500

    initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from wordcounter import wordcounter_app
 from weatherapp import weatherapp_app
 from languagetranslator import languagetranslator_app
 from currencyconverter import currencyconverter_app
-
+from imageconverter import imageconverter_app
 
 app = Flask(__name__)
 
@@ -14,6 +14,7 @@ app.register_blueprint(wordcounter_app, url_prefix="/wordcounter")
 app.register_blueprint(weatherapp_app, url_prefix="/weatherapp")
 app.register_blueprint(languagetranslator_app, url_prefix="/languagetranslator")
 app.register_blueprint(currencyconverter_app, url_prefix="/currencyconverter") 
+app.register_blueprint(imageconverter_app, url_prefix="/imageconverter")
 
 if __name__ == '__main__':
   app.run(debug=True)
