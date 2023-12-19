64ca766107e7d9004beb5c60d56d6a5e69bafac7
muhammad fahad <muhammadfahadnaeem4@gmail.com>
2023-12-19 09:33:04 +0500

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from wordcounter import wordcounter_app
 from weatherapp import weatherapp_app
-from languagetranslator import languagetranslator_app
 from currencyconverter import currencyconverter_app
 
 
@@ -12,7 +11,6 @@ def index():
     return render_template('index.html')
 app.register_blueprint(wordcounter_app, url_prefix="/wordcounter")
 app.register_blueprint(weatherapp_app, url_prefix="/weatherapp")
-app.register_blueprint(languagetranslator_app, url_prefix="/languagetranslator")
 app.register_blueprint(currencyconverter_app, url_prefix="/currencyconverter") 
 
 if __name__ == '__main__':
