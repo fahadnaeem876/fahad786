f6585bdeb48776ae7325e2d8722d9cd2b5b3cbeb
muhammad fahad <muhammadfahadnaeem4@gmail.com>
2023-12-19 11:48:54 +0500

    initial commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from wordcounter import wordcounter_app
 from weatherapp import weatherapp_app
 from currencyconverter import currencyconverter_app
-from imageconverter import imageconverter_app
 
 app = Flask(__name__)
 
@@ -13,7 +12,6 @@ def index():
 app.register_blueprint(wordcounter_app, url_prefix="/wordcounter")
 app.register_blueprint(weatherapp_app, url_prefix="/weatherapp")
 app.register_blueprint(currencyconverter_app, url_prefix="/currencyconverter")
-app.register_blueprint(imageconverter_app, url_prefix="/imageconverter")  
 
 if __name__ == '__main__':
     try:
