30dc751605daeadbdeea83f7d29a04e8a876ecb7
muhammad fahad <muhammadfahadnaeem4@gmail.com>
2023-12-19 12:07:47 +0500

    initial commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from wordcounter import wordcounter_app
 from weatherapp import weatherapp_app
 from currencyconverter import currencyconverter_app
+from languagetranslator import languagetranslator_app
 
 app = Flask(__name__)
 
@@ -12,9 +13,10 @@ def index():
 app.register_blueprint(wordcounter_app, url_prefix="/wordcounter")
 app.register_blueprint(weatherapp_app, url_prefix="/weatherapp")
 app.register_blueprint(currencyconverter_app, url_prefix="/currencyconverter")
+app.register_blueprint(languagetranslator_app, url_prefix="/languagetranslator")  
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        app.run(port=8004,debug=True)
     except Exception as e:
         print(f"An error occurred: {e}")
